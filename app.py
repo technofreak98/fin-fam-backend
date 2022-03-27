986140895df04d2dca291403725ad95b93286d01
Naveen Kumar <navis9991@gmail.com>
2022-03-27 08:46:20 +0530

    Added app

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 app.register_blueprint(users.devices_bp)
 app.register_blueprint(investments.devices_bp)
 
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
 if __name__ == "__main__":
   app.run()
